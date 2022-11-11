e839a7e0117ef3d36f38ec352971a9be4f057a48
GITED9 <edenmaimon9@gmail.com>
2022-11-11 14:29:44 +0200

    Fixing rectangle shuffling

--- RectangleShuffleing.py
+++ RectangleShuffleing.py
@@ -2,15 +2,11 @@ import sys
 import math
 import random
 from random import randrange
-from SimpleDiseaseGeneratorByGene import DisaeseModel
-from ComplexDiseaseGeneragor import ComplexDisaeseModel
 from itertools import islice
 import functools
 import operator
 import statistics
-import matplotlib.pyplot as plt
 import numpy as np
-import subprocess
 import logging
 import os
 
@@ -36,62 +32,17 @@ def randomizeIndex(ncol, nrow):
 
 	return gene1,gene2,genome1
 
-def createPathsInGenome(genome,paths):
-	genome = iter(genome) 
-	Output = [list(islice(genome, elem)) for elem in paths] 
-
-	return Output
-
-def writeNewMatrixToAfile(file_name, matrix, first_line,disease):
-	
-	output_file = open(file_name, "w+")
-
-	#If the desctiption line did not include the "health status", add it
-	if not first_line.strip().endswith("status"):
-		first_line=first_line.strip()+",health status\n"
-
-	output_file.write(first_line)
-
-	healthStatus_counter={"Healthy":0,"Sick":0}
-
-	for genome in matrix:
-			
-			genome=createPathsInGenome(genome,disease.paths)
-			
-			#print("Now")
-			healthStatus = disease.healthStatus(genome)
-			
-			healthStatus_counter[healthStatus]+=1
-
-			line = ",".join(str(x) for x in functools.reduce(
-				operator.iconcat, genome, []))
-
-			output_file.write(line + "," + healthStatus + "\n")
-
-	output_file.close()
-
-	return healthStatus_counter
-
 
 def readOriginalDataIntoMartix(input_file_name):
 
-
-	try:
-		# Reading the base data
-		base_data_file = open(input_file_name, "r")
-		
-	except IOError:
-		print(f"ahmm, Eden? don't kill me but... {input_file_name} is not a real file....")
-		exit()
-		
-
+	# Reading the base data
+	base_data_file = open(input_file_name, "r")
 
 	# The matrix that holds the data as list of lists
 	base_data_matrix = []
 
 	# Is this the first line? if it is-> skip it.
 	first_line_flag = True
-
 
 	# Create a matrix that holds all the data from the base data set.
 
@@ -103,23 +54,21 @@ def readOriginalDataIntoMartix(input_file_name):
 			first_line_flag = False
 			continue
 
-		
 		# make a list of each line (corresponding to a genome)
 		line = line.strip().split(",")
 
 		#In case the data contatins already a health status- delete it.
-		if line[-1]=="Healthy" or line[-1]=="Sick":
+		if line[-1] == "Healthy" or line[-1] == "Sick":
 			del line[-1]
-
 
 		# Add the list created as list of ints
 		base_data_matrix.append([int(x) for x in line])
-
-
+		
 	return base_data_matrix,first_line
 
+
 def shuffleData(input_file_name):
-	#subprocess.call('echo "Shuffeling has started." | mail -s "CreateDiseaseByShuffeling" edenmaimon6@gmail.com', shell=True)
+
 	base_data_matrix,first_line = readOriginalDataIntoMartix(input_file_name)
 	# Size of the matrix= number of inner lists X the len of an inner list (as
 	# they all in the same length).
@@ -147,7 +96,6 @@ def shuffleData(input_file_name):
 
 	# Number of shuffles required =size*log2(size):
 	num_of_shuffle = total_size_of_matrix * math.log2(total_size_of_matrix)
-	#num_of_shuffle = 200
 
 	shuffles_done = 0
 
@@ -188,152 +136,29 @@ def shuffleData(input_file_name):
 	#plt.plot(genomes_matches_through_shuffeling,'ro')
 	#plt.savefig("twoGenomesMatch.png")
 	
-	#subprocess.call('echo "Shuffeling is done." | mail -s "CreateDiseaseByShuffeling" edenmaimon6@gmail.com', shell=True)
-	
 	return base_data_matrix,first_line
 
 
-
-
-"""
-Fileds of the model
-paths, numberOfSubTypes,, maxDefectiveGenes=3, maxDefectivePaths=2,zeroDes, oneDes, TwoDes, 
-		alloweDeviation=100
-"""
-def createSimpleDiseaseModel(paths):
-	"""Create a disase model fit for a simple models.
-	"""
-	maxDefectiveGenes=float(sys.argv[3])
-	maxDefectivePaths=float(sys.argv[4])
-	numberOfSubTypes=int(sys.argv[5])
-
-	disease = DisaeseModel(paths, numberOfSubTypes,maxDefectiveGenes,maxDefectivePaths)
-
-	return disease
-
-def createComplexDisaseModel(paths):
+def writeNewMatrixToAfile(file_name, matrix):
 	
-	"""Create a disase model fit for a complex models:
+  output_file = open(file_name, "w+")
+ 
+  for genome in matrix:
+    
+    line = ",".join(str(x) for x in genome) + "\n"
+    
+    output_file.write(line)
 
-		self, paths, numberOfCoreGenes, numOfCorePaths, weightOfRegularGenes , weightOfRegularPaths 
-		, weightOfCorePaths , weightOfCoreGenes, maxPaths , maxGenes , numberOfSubTypes = 1 ,
-		zeroOdd = 0,oneOdds = 0, twoOdds = 0 ,zeroDes = zeroDes, oneDes = oneDes, TwoDes = TwoDes, alloweDeviation = 0
-	
-	"""
-	
-	
-	"""
-	maxGenes = 23
-	maxPaths = 11
-	
-
-	numOfCorePaths = 3
-	weightOfCorePaths = 5
-
-	numberOfCoreGenes = [3] * 8
-	weightOfCoreGenes = 5
-
-	#Genes
-	weightOfRegularGenes = 2 
-	
-
-	#Paths
-	weightOfRegularPaths = 2.2
-	
-	
-
-	number_Of_SubTypes = 1
-	
-	"""
-
-	numberOfGenes = int(sys.argv[3])
-	numberOfPaths = int(sys.argv[4])
-	
-	maxGenes = float(sys.argv[5])
-	maxPaths = float(sys.argv[6])
-
-	paths= [numberOfGenes] * numberOfPaths
+  output_file.close()
 
 
-	numOfCorePath = int(sys.argv[7])
-	corePathWeight = float(sys.argv[8])
-
-	numOfCoreGenes = [int(sys.argv[9])] * numberOfPaths
-	coreGeneWeight = float(sys.argv[10])
-
-	regGeneWeight = float(sys.argv[11])
-
-	number_Of_SubTypes = 1
-	#regPathWeight = maxPaths / number of regular genes
-	regPathWeight = (maxPaths / (numberOfPaths - numOfCorePath))
-	#regPathWeight = 1
-	#print(f"numberOfGenes :{numberOfGenes}\n numberOfPaths: {numberOfPaths}\n  maxGenes: {maxGenes}")
-	#print(f"maxPaths :{maxPaths}\n numOfCorePath: {numOfCorePath}\n  corePathWeight: {corePathWeight}")
-	#print(f"numOfCoreGenes :{numOfCoreGenes}\n coreGeneWeight: {coreGeneWeight}\n  regGeneWeight: {regGeneWeight}\n")
-
-	disease = ComplexDisaeseModel(paths, numOfCoreGenes, numOfCorePath, regGeneWeight,
-		 regPathWeight, corePathWeight , coreGeneWeight, maxPaths, maxGenes, number_Of_SubTypes)
-	random.seed()
-	return disease
 
 if __name__ == '__main__':
 	
-	#Is the disease complex or simple?
-	type_of_disease_model = sys.argv[1]
-	#The name of the file to be shuffled
-	input_file_name = sys.argv[2]
-	#[number of genes in each path] * number of paths
-	paths = [6]*8
-
-	#Create the model and extract the parameters accorrding to weather the disease is simple or complex.
-	if (type_of_disease_model == "Simple" ) :
-		output_file = sys.argv[6]
-		disease = createSimpleDiseaseModel(paths)
-
-	elif (type_of_disease_model == "Complex" ):
-		output_file = sys.argv[12]
-		disease = createComplexDisaseModel(paths)
 	
-	shuffled_matrix,first_line = shuffleData(input_file_name)
-	
-	#file_name, matrix, first_line,disease
-	health_Status_counter = writeNewMatrixToAfile(output_file,shuffled_matrix,first_line,disease)
-
-	print("Health status", health_Status_counter)
-
-	print("Sick precentage", (health_Status_counter["Sick"] / (health_Status_counter["Sick"] + health_Status_counter["Healthy"]))*100)
-	print("Healthy precentage", (health_Status_counter["Healthy"] / (health_Status_counter["Sick"] + health_Status_counter["Healthy"]))*100)
+	input_file = sys.argv[1]
+	#input_file = '/content/RealDataCombined_comma.csv'
+	base_data_matrix,first_line = shuffled_matrix,first_line = shuffleData(input_file)
+	writeNewMatrixToAfile(file_name, matrix)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-"""
-COMMAND LINE PARAMETERS SIMPLE: 
-sys.argv[1]= type_of_disease_model (Simple)
-sys.argv[2]= file name of the base Data to be shuffled.
-sys.argv[3]= maxDefectiveGenes
-sys.argv[4]= maxDefectivePaths
-sys.argv[5]= numberOfSubTypes
-sys.argv[6]= output file name
-"""
-
-"""
-COMMAND LINE PARAMETERS Complex: 
-sys.argv[1]= type_of_disease_model (Complex)
-sys.argv[2]= file name of the base Data to be shuffled.
-sys.argv[3]= numberOfGenes
-sys.argv[4]= numberOfPaths
-sys.argv[5]= maxGenes
-sys.argv[6]= maxPaths 
-sys.argv[7]= numOfCorePath
-sys.argv[8]= corePathWeight
-sys.argv[9]= numOfCoreGenes
-sys.argv[10]= coreGeneWeight
-sys.argv[11]= regGeneWeight
-sys.argv[12]= output file name
-"""
-
-
-
-
-
-
